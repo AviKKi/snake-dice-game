e938e4b058626815922d21b5a811baa28f47d828
avikki <atul7555@gmail.com>
2022-06-19 05:24:58 +0530

    snaked added

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   const allRows = Array(10)
     .fill(0)
     .map((_, rowIdx) => <BoardRow key={rowIdx} rowIdx={9 - rowIdx} />);
-  const { rollDice, dice, marblePosition, reset } = useGameState();
+  const { rollDice, dice, marblePosition, reset, diceLocked } = useGameState(true);
   const marbleStyle = useMarbleStyle(marblePosition, boardRef);
 
   return (
@@ -20,9 +20,14 @@ function App() {
       <div ref={boardRef} className="boardBody">
         {allRows}
         <div style={marbleStyle} className="marble"></div>
+        <div>
+          <img className="snake snake1" src="snake-1.png"/>
+          <img className="snake snake2" src="snake-2.png"/>
+          <img className="snake snake3" src="snake-3.png"/>
+        </div>
       </div>
       <img
-        className="dice"
+        className={`dice ${diceLocked&&"diceLocked"}`}
         src={`/dice-${dice}.png`}
         alt="die"
         onClick={rollDice}
--- src/hooks/useGameState.tsx
+++ src/hooks/useGameState.tsx
@@ -1,44 +1,82 @@
 import { useEffect, useState } from "react";
 
-export default function useGameState() {
+// 0-indexed mapping for snake-head: snake-tail
+const SNAKES: { [key: number]: number } = {
+  98: 61,
+  89: 45,
+  11: 8
+};
+const TRANSITION_DELAY = 1000;
+
+/**
+ *
+ * @param lockableDice a lock on the rollDice function, which waits for marble movement until next roll is done
+ * @returns
+ */
+export default function useGameState(lockableDice?: boolean) {
   // history of the Game
   let history: { newPosition: number; dice: number }[] = [];
-  const [marblePosition, setMarblePosition] = useState(-1);
+  const [marblePosition, setMarblePosition] = useState({ value: -1 });
   const [dice, setDice] = useState(1);
-  // timestamp of last dice roll, to move marble even if 2 consecutive throws are same
-  const [seed, setSeed] = useState(new Date());
+  const [diceLocked, setDiceLocked] = useState(false);
+  // used to trigger useEffect when value of dice is same in consecutive rolls
+  // const [seed, setSeed] = useState(new Date());
+
   /**
-   * Roll the dice
+   * Roll the dice and update position
    */
   function rollDice() {
+    if (lockableDice && diceLocked) return;
     const randomDice = Math.floor(6 * Math.random()) + 1;
     setDice(randomDice);
-    setSeed(new Date());
+    // setSeed(new Date());
+    updatePosition(randomDice);
+    setDiceLocked(true);
+    // setSeed(new Date());
   }
 
   /**
    * Update the marble position whever dice is rolled
    */
-  useEffect(() => {
-    let newPosition = marblePosition;
-    if (marblePosition === -1 && dice === 6) {
+  function updatePosition(randomDice: number) {
+    let newPosition = marblePosition.value;
+    if (marblePosition.value === -1 && randomDice === 6) {
       // setMarblePosition(0);
       newPosition = 0;
-    } else if (marblePosition > -1) {
-      if (marblePosition + dice < 100) newPosition = marblePosition + dice;
-      else newPosition = marblePosition;
+    } else if (marblePosition.value > -1) {
+      if (marblePosition.value + randomDice < 100)
+        newPosition = marblePosition.value + randomDice;
+      else newPosition = marblePosition.value;
     }
-    setMarblePosition(newPosition);
-    history.push({ dice, newPosition });
-  }, [dice, setMarblePosition, seed]);
+    setMarblePosition({ value: newPosition });
+    history.push({ dice: randomDice, newPosition });
+  }
 
+  // check for snakes for new dice position, with a delay
+  useEffect(() => {
+    const snakeEnd = SNAKES[marblePosition.value];
+    if (marblePosition.value > -1) {
+      const id = setTimeout(() => {
+        if (snakeEnd) setMarblePosition({ value: snakeEnd });
+        setDiceLocked(false);
+      }, TRANSITION_DELAY);
+      return () => clearTimeout(id);
+    } else setDiceLocked(false);
+  }, [marblePosition, setDiceLocked]);
+  console.log(diceLocked);
   /**
    * Reset the game state to starting position
    */
   function reset() {
     setDice(1);
-    setMarblePosition(-1);
+    setMarblePosition({ value: -1 });
   }
 
-  return { marblePosition, rollDice, reset, dice };
+  return {
+    marblePosition: marblePosition.value,
+    rollDice,
+    reset,
+    dice,
+    diceLocked,
+  };
 }
